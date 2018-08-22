f0ca13fda84a2f22e99ff23276d8482e2d8097a4
Monty Anderson <work@montyanderson.net>
2018-08-22 13:05:05 +0100

    Attempt at multi-block snake logic

--- server.js
+++ server.js
@@ -5,10 +5,15 @@ let id = 0;
 
 io.on('connection', socket => {
 	const snake = {
-		x: 5,
-		y: 5,
-		direction: 'up',
-		id: id++
+		blocks: [
+			{
+				x: 5,
+				y: 5,
+				direction: 'up'
+			}
+		],
+		id: id++,
+		counter: 0
 	};
 
 	snakes.push(snake);
@@ -17,7 +22,7 @@ io.on('connection', socket => {
 	socket.emit('snakes', snakes);
 
 	socket.on('direction', direction => {
-		snake.direction = direction;
+		snake.blocks[0].direction = direction;
 	});
 
 	socket.on('disconnect', () => {
@@ -31,24 +36,72 @@ setInterval(() => {
 	const canvasBox = 10;
 
 	for(const snake of snakes) {
-		({
-			'up': () => {
-				if(--snake.x === 0)
-					snake.direction = 'down';
-			},
-			'down': () => {
-				if(++snake.x === canvasBox - 1)
-					snake.direction = 'up';
-			},
-			'left': () => {
-				if(--snake.y === 0)
-					snake.direction = 'right';
-			},
-			'right': () => {
-				if(++snake.y === canvasBox - 1)
-					snake.direction = 'left';
-			}
-		})[snake.direction]();
+		if(snake.counter++ % 5 === 0) {
+			const lastBlock = snake.blocks[snake.blocks.length - 1];
+
+			console.log(lastBlock.direction);
+
+			const block = ({
+				'up': () => ({
+					x: lastBlock.x - 1,
+					y: lastBlock.y,
+					direction: 'up'
+				}),
+				'down': () => ({
+					x: lastBlock.x + 1,
+					y: lastBlock.y,
+					direction: 'down'
+				}),
+				'left': () => ({
+					x: lastBlock.x,
+					y: lastBlock.y + 1,
+					direction: 'left'
+				}),
+				'right': () => ({
+					x: lastBlock.x,
+					y: lastBlock.y - 1,
+					direction: 'right'
+				})
+			})[lastBlock.direction]();
+
+			snake.blocks.push(block);
+		}
+
+		if(snake.blocks.length >= 2) {
+			(() => {
+				for(let i = 0; i < snake.blocks.length - 3; i++) {
+					const blocks = snake.blocks.slice(i, 2);
+									console.log(blocks);
+
+					if(blocks[0].direction !== blocks[1].direction) {
+						blocks[0].direction = blocks[1].direction;
+
+						return;
+					}
+				}
+			})();
+		}
+
+		for(const block of snake.blocks) {
+			({
+				'up': () => {
+					if(--block.x === 0)
+						block.direction = 'down';
+				},
+				'down': () => {
+					if(++block.x === canvasBox - 1)
+						block.direction = 'up';
+				},
+				'left': () => {
+					if(--block.y === 0)
+						block.direction = 'right';
+				},
+				'right': () => {
+					if(++block.y === canvasBox - 1)
+						block.direction = 'left';
+				}
+			})[block.direction]();
+		}
 	}
 
 	io.emit('snakes', snakes);
