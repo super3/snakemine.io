444f0e82e99c0a70e7df8e03b078061d02fb14e0
Monty Anderson <work@montyanderson.net>
2018-08-23 11:25:10 +0100

    Entity system

--- server.js
+++ server.js
@@ -1,33 +1,24 @@
 const io = require('socket.io')(3055);
 
-const snakes = new Set();
+const Snake = require('./lib/Snake');
+
+const entities = new Set();
 let id = 0;
 
 io.on('connection', socket => {
-	const snake = {
-		blocks: [
-			{
-				x: 12,
-				y: 12,
-				direction: 'up'
-			}
-		],
-		direction: 'up',
-		id: id++,
-		counter: 0
-	};
+	const snake = new Snake();
 
-	snakes.add(snake);
+	entities.add(snake);
 
 	socket.emit('snakeId', snake.id);
-	socket.emit('snakes', [ ...snakes ]);
+	socket.emit('entities', [ ...entities ]);
 
 	socket.on('direction', direction => {
 		snake.direction = direction;
 	});
 
 	socket.on('disconnect', () => {
-		snakes.delete(snake);
+		entities.delete(snake);
 	});
 });
 
@@ -36,58 +27,31 @@ const fps = 3.5;
 setInterval(() => {
 	const canvasBox = 25;
 
-	for(const snake of snakes) {
-		if(snake.counter++ % 5 === 0) {
-			const lastBlock = snake.blocks[snake.blocks.length - 1];
+	for(const entity of entities) {
+		if(entity instanceof Snake) {
+			if(entity.counter++ % 5 === 0) {
+				entity.appendBlock();
+			}
 
-			console.log(lastBlock.direction);
+			entity.shiftState();
 
-			const block = ({
-				'up': () => ({
-					x: lastBlock.x + 1,
-					y: lastBlock.y,
-					direction: 'up'
-				}),
-				'down': () => ({
-					x: lastBlock.x - 1,
-					y: lastBlock.y,
-					direction: 'down'
-				}),
-				'left': () => ({
-					x: lastBlock.x,
-					y: lastBlock.y + 1,
-					direction: 'left'
-				}),
-				'right': () => ({
-					x: lastBlock.x,
-					y: lastBlock.y - 1,
-					direction: 'right'
-				})
-			})[lastBlock.direction]();
+			for(const block of entity.blocks) {
+				({
+					'up': () => --block.x,
+					'down': () => ++block.x,
+					'left': () => --block.y,
+					'right': () => ++block.y
+				})[block.direction]();
+			}
 
-			snake.blocks.push(block);
+			const { x, y } = entity.blocks[0];
+
+			if(x === 0 || x === canvasBox || y === 0 || y === canvasBox) {
+				entities.delete(entity);
+				entities.add(...entity.toFood());
+			}
 		}
-
-		for(let i = snake.blocks.length - 1; i >= 1; i--) {
-			snake.blocks[i].direction = snake.blocks[i - 1].direction;
-		}
-
-		snake.blocks[0].direction = snake.direction;
-
-		for(const block of snake.blocks) {
-			({
-				'up': () => --block.x,
-				'down': () => ++block.x,
-				'left': () => --block.y,
-				'right': () => ++block.y
-			})[block.direction]();
-		}
-
-		const { x, y } = snake.blocks[0];
-
-		if(x === 0 || x === canvasBox || y === 0 || y === canvasBox)
-			snakes.delete(snake);
 	}
 
-	io.emit('snakes', [ ...snakes ]);
+	io.emit('entities', [ ...entities ]);
 }, 1000 / fps);
