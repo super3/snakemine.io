fa9cc4b6158fe7f5d6800a8942fd685a8799aa17
Monty Anderson <work@montyanderson.net>
2018-08-23 11:57:40 +0100

    Semi-working collision-detection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const io = require('socket.io')(3055);
 
 const Snake = require('./lib/Snake');
+const detectCollisions = require('./lib/detectCollisions');
 
 const entities = new Set();
 let id = 0;
@@ -25,11 +26,11 @@ io.on('connection', socket => {
 const fps = 3.5;
 
 setInterval(() => {
-	const canvasBox = 25;
+	const canvasBox = 24;
 
 	for(const entity of entities) {
-		if(entity instanceof Snake) {
-			if(entity.counter++ % 5 === 0) {
+		if(entity.type === 'snake') {
+			if(++entity.counter % 5 === 0) {
 				entity.appendBlock();
 			}
 
@@ -46,9 +47,44 @@ setInterval(() => {
 
 			const { x, y } = entity.blocks[0];
 
-			if(x === 0 || x === canvasBox || y === 0 || y === canvasBox) {
+			if(x === 0 || x >= canvasBox || y === 0 || y >= canvasBox) {
 				entities.delete(entity);
 				entities.add(...entity.toFood());
+			}
+		}
+	}
+
+	for(const [ a, b ] of detectCollisions(entities)) {
+		if(!(entities.has(a) && entities.has(b))) {
+			// entities may have been deleted
+			return;
+		}
+
+		if(a.type === 'food' && b.type === 'food') {
+			return;
+		}
+
+		if(a.type === 'snake' && b.type === 'snake') {
+			if(a === b) {
+				// snake crossing itself
+				return;
+			}
+
+			if(a.blocks.length === b.blocks.length) {
+				// snakes of equal length
+				return;
+			}
+
+			if(a.blocks.length > b.blocks.length) {
+				// snake a is bigger
+				entities.delete(b);
+				entities.add(...b.toFood());
+			}
+
+			if(a.blocks.length < b.blocks.length) {
+				// snake b is bigger
+				entites.delete(a);
+				entites.add(...a.toFood());
 			}
 		}
 	}
