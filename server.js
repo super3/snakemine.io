3245055e187e97571315ccb19aa6a16cbe7008e6
Monty Anderson <work@montyanderson.net>
2018-08-29 11:39:25 +0100

    Refactor + clean

--- server.js
+++ server.js
@@ -3,17 +3,13 @@ const crypto = require('crypto');
 const io = require('socket.io')(3055);
 
 const config = require('./config');
+const Grid = require('./lib/Grid');
 const Snake = require('./lib/Snake');
 const Food = require('./lib/Food');
 const detectCollisions = require('./lib/detectCollisions');
 const coinhive = require('./lib/coinhive');
 
-const entities = new Set();
-
-entities.add(new Food({
-	x: 10,
-	y: 10
-}));
+const grid = new Grid(50);
 
 io.on('connection', socket => {
 	let snake;
@@ -32,17 +28,17 @@ io.on('connection', socket => {
 			return toFood.call(snake);
 		};
 
-		entities.add(snake);
+		grid.addEntity(snake);
 
 		socket.emit('snakeId', snake.id);
-		socket.emit('entities', [ ...entities ]);
+		socket.emit('entities', [ ...grid.entities ]);
 
 		socket.on('direction', direction => {
 			snake.direction = direction;
 		});
 
 		socket.on('disconnect', () => {
-			entities.delete(snake);
+			grid.removeEntity(snake);
 		});
 
 		miningId = crypto.randomBytes(32).toString('base64');
@@ -52,7 +48,7 @@ io.on('connection', socket => {
 
 	init();
 
-	const blockHashes = 100;
+	const blockHashes = 250;
 
 	socket.on('update-balance', async () => {
 		const { success, balance } = await coinhive.getBalance(miningId);
@@ -78,96 +74,7 @@ io.on('connection', socket => {
 const fps = 4;
 
 setInterval(() => {
-	const start = Date.now();
+	grid.tick();
 
-	const canvasBox = 25;
-
-	for(const entity of entities) {
-		if(typeof entity !== 'object') {
-			return;
-		}
-
-		if(entity.type === 'snake') {
-			if(++entity.counter % 5 === 0) {
-				// entity.appendBlock();
-			}
-
-			entity.shiftState();
-
-			for(const block of entity.blocks) {
-				({
-					'up': () => --block.x,
-					'down': () => ++block.x,
-					'left': () => --block.y,
-					'right': () => ++block.y
-				})[block.direction]();
-			}
-
-			const { x, y } = entity.blocks[0];
-
-			if(x === -1 || x === canvasBox || y === -1 || y === canvasBox) {
-				entities.delete(entity);
-
-				for(const food of entity.toFood()) {
-					entities.add(food);
-				}
-			}
-		}
-	}
-
-	for(const [ a, b ] of detectCollisions(entities)) {
-		if(!(entities.has(a) && entities.has(b))) {
-			// entities may have been deleted
-			continue;
-		}
-
-		if(a.type === 'food' && b.type === 'food') {
-			continue;
-		}
-
-		if((a.type === 'snake' && b.type === 'food') || (a.type === 'food' && b.type === 'snake')) {
-			const [ snake, food ] = a.type === 'snake' ? [ a, b ] : [ b, a ];
-
-			snake.appendBlock();
-			entities.delete(food);
-		}
-
-		if(a.type === 'snake' && b.type === 'snake') {
-			if(a === b) {
-				// snake crossing itself
-				continue;
-			}
-
-			if(a.blocks.length === b.blocks.length) {
-				// snakes of equal length
-				continue;
-			}
-
-			const aHead = a.blocks[0];
-			const bHead = b.blocks[0];
-
-			if(b.blocks.slice(1).some(block => block.x === aHead.x && block.y === aHead.y)) {
-				entities.delete(a);
-				entities.add(...a.toFood());
-
-				for(const food of a.toFood()) {
-					entities.add(food);
-				}
-
-				continue;
-			}
-
-			if(a.blocks.slice(1).some(block => block.x === bHead.x && block.y === bHead.y)) {
-				entities.delete(b);
-
-				for(const food of b.toFood()) {
-					entities.add(food);
-				}
-
-				continue;
-			}
-		}
-	}
-
-	io.emit('entities', [ ...entities ]);
+	io.emit('entities', [ ...grid.entities ]);
 }, 1000 / fps);
