bdf7cdefe3697019db51e2e3402079f6cded9e13
Monty Anderson <work@montyanderson.net>
2018-08-30 17:40:18 +0100

    public/private key system

--- server.js
+++ server.js
@@ -12,47 +12,55 @@ const coinhive = require('./lib/coinhive');
 const grid = new Grid(50);
 
 io.on('connection', socket => {
-	const snake = new Snake();
 
-	grid.addEntity(snake);
+	socket.on('init', privateKey => {
+		const publicKey = (() => {
+			const hash = crypto.createHash('sha256');
 
-	socket.emit('snakeId', snake.id);
-	socket.emit('entities', [ ...grid.entities ]);
+			hash.update(privateKey, 'hex');
 
-	socket.on('direction', direction => {
-		snake.direction = direction;
+			return hash.digest('hex');
+		})();
+
+		console.log({ privateKey, publicKey });
+
+		const snake = new Snake();
+
+		grid.addEntity(snake);
+
+		socket.emit('snakeId', snake.id);
+		socket.emit('entities', [ ...grid.entities ]);
+
+		socket.on('direction', direction => {
+			snake.direction = direction;
+		});
+
+		socket.on('disconnect', () => {
+			grid.removeEntity(snake);
+		});
+
+		socket.emit('mining-id', process.env.COINHIVE_SITE_KEY);
+
+		const blockHashes = 250;
+
+		socket.on('update-balance', async () => {
+			const { success, balance } = await coinhive.getBalance(publicKey);
+
+			if(success !== true)
+				return;
+
+			const newBlocks = Math.floor(balance / blockHashes);
+
+			if(newBlocks <= 1)
+				return;
+
+			await coinhive.withdraw(miningId, newBlocks * blockHashes);
+
+			for(let i = 0; i < newBlocks; i++)
+				snake.appendBlock();
+		});
 	});
 
-	socket.on('disconnect', () => {
-		grid.removeEntity(snake);
-	});
-
-	const miningId = crypto.randomBytes(32).toString('base64');
-
-	socket.emit('mining-id', process.env.COINHIVE_SITE_KEY, miningId);
-
-
-	const blockHashes = 250;
-
-	socket.on('update-balance', async () => {
-		const { success, balance } = await coinhive.getBalance(miningId);
-
-		if(success !== true) {
-			return;
-		}
-
-		const newBlocks = Math.floor(balance / blockHashes);
-
-		if(newBlocks <= 1) {
-			return;
-		}
-
-		await coinhive.withdraw(miningId, newBlocks * blockHashes);
-
-		for(let i = 0; i < newBlocks; i++) {
-			snake.appendBlock();
-		}
-	});
 });
 
 const fps = 4;
